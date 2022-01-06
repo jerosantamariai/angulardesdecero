13dc1c3b26498f28b03ad6a7ee4fd6923a149411
jerosantamariai <jerosantamariai@gmail.com>
2022-01-05 21:40:35 -0300

    videos until 20

---
+++ seccion3/ejercicio_s3v13_introts/src/ejercicios/04-tarea-tipado.ts
@@ -0,0 +1,42 @@
+
+interface SuperHeroe {
+    nombre:string;
+    edad:number;
+    direccion: {
+        calle:string;
+        pais:string;
+        ciudad:string;
+    },
+    mostrarDireccion: () => string;
+}
+
+// OTRA ALTERNATIVA ES:
+
+// interface SuperHeroe {
+//     nombre:string;
+//     edad:number;
+//     direccion: Direccion,
+//     mostrarDireccion: () => string;
+// }
+
+// interface Direccion {
+//     calle:string;
+//     pais:string;
+//     ciudad:string;
+// }
+
+const superHeroe:SuperHeroe = {
+    nombre: 'Spiderman',
+    edad: 30,
+    direccion: {
+        calle: 'Main St',
+        pais: 'USA',
+        ciudad: 'NY'
+    },
+    mostrarDireccion() {
+        return this.nombre + ', ' + this.direccion.ciudad + ', ' + this.direccion.pais;
+    }
+}
+
+const direccion = superHeroe.mostrarDireccion();
+console.log(direccion);
