179cf9dbed52866f8504bac44d3c88b689ac4090
jerosantamariai <jerosantamariai@gmail.com>
2022-02-23 23:31:57 -0300

    first components from primeng

--- 10seccion/04pipesApp/src/app/app.module.ts
+++ 10seccion/04pipesApp/src/app/app.module.ts
@@ -3,12 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 
+import {ButtonModule} from 'primeng/button';
+import {CardModule} from 'primeng/card';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonModule,
+    CardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
