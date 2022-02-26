f868cd72ab756e3186eec554f440cdc2ee174958
jerosantamariai <jerosantamariai@gmail.com>
2022-02-25 22:50:47 -0300

    ordering codes of primeng

---
+++ 10seccion/04pipesApp/src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+
+// PrimeNg
+import {ButtonModule} from 'primeng/button';
+import {CardModule} from 'primeng/card';
+
+
+@NgModule({
+  declarations: [],
+  imports: [],
+  exports: [
+    ButtonModule,
+    CardModule
+  ]
+})
+export class PrimeNgModule { }
