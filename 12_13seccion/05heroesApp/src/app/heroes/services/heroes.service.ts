e9be80506edce366de8f1132905931748f6950ea
jerosantamariai <jerosantamariai@gmail.com>
2022-02-27 18:04:55 -0300

    until video 190

---
+++ 12_13seccion/05heroesApp/src/app/heroes/services/heroes.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Heroe } from '../interfaces/heroes.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroesService {
+
+  constructor( private http: HttpClient ) {
+   
+  }
+
+  getHeroes(): Observable<Heroe[]> {
+      return this.http.get<Heroe[]>('http://localhost:3000/heroes')
+  }
+}
