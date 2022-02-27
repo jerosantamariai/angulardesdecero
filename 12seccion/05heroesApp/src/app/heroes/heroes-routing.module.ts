9448b745e5e423ae4a475c3f5b50880d466bdfd0
jerosantamariai <jerosantamariai@gmail.com>
2022-02-27 13:40:21 -0300

    end of section 12

--- 12seccion/05heroesApp/src/app/heroes/heroes-routing.module.ts
+++ 12seccion/05heroesApp/src/app/heroes/heroes-routing.module.ts
@@ -5,10 +5,12 @@ import { ListadoComponent } from './pages/listado/listado.component';
 import { AgregarComponent } from './pages/agregar/agregar.component';
 import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
+import { HomeComponent } from './pages/home/home.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: HomeComponent,
     children: [
       {
         path: 'listado',
