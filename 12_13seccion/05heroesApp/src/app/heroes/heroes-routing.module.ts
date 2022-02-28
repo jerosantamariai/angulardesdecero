db07a871182c84fcf984faa060782c8b90c76d26
jerosantamariai <jerosantamariai@gmail.com>
2022-02-28 00:05:06 -0300

    adding section13

--- 12_13seccion/05heroesApp/src/app/heroes/heroes-routing.module.ts
+++ 12_13seccion/05heroesApp/src/app/heroes/heroes-routing.module.ts
@@ -7,6 +7,7 @@ import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 
+
 const routes: Routes = [
   {
     path: '',
--- 12_13seccion/05heroesApp/src/app/heroes/services/heroes.service.ts
+++ 12_13seccion/05heroesApp/src/app/heroes/services/heroes.service.ts
@@ -3,17 +3,25 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { Heroe } from '../interfaces/heroes.interface';
+// import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroesService {
 
+  // private baseURL: string = environment.baseURL;
+
   constructor( private http: HttpClient ) {
    
   }
 
   getHeroes(): Observable<Heroe[]> {
-      return this.http.get<Heroe[]>('http://localhost:3000/heroes')
+      return this.http.get<Heroe[]>(`http://localhost:3000/heroes`)
+  }
+
+  getHeroeoPorId( id: string ): Observable<Heroe> {
+    return this.http.get<Heroe>(`http://localhost:3000/heroes/${ id }`);
+
   }
 }
