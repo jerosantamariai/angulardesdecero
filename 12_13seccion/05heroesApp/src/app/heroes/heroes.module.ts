92066d09928bc7d42c41c6fcb40f717d07d2989a
jerosantamariai <jerosantamariai@gmail.com>
2022-02-27 21:15:24 -0300

    until video 194

--- 12_13seccion/05heroesApp/src/app/heroes/heroes.module.ts
+++ 12_13seccion/05heroesApp/src/app/heroes/heroes.module.ts
@@ -10,6 +10,8 @@ import { BuscarComponent } from './pages/buscar/buscar.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
+import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
+import { ImagenPipe } from './pipes/imagen.pipe';
 
 
 
@@ -19,7 +21,9 @@ import { ListadoComponent } from './pages/listado/listado.component';
     BuscarComponent,
     HeroeComponent,
     HomeComponent,
-    ListadoComponent
+    ListadoComponent,
+    HeroeTarjetaComponent,
+    ImagenPipe
   ],
   imports: [
     CommonModule,
