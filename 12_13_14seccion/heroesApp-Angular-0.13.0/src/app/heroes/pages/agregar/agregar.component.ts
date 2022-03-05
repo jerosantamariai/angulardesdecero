f8ffe901edc03aa28644a4f995fa96e65c4c6588
jerosantamariai <jerosantamariai@gmail.com>
2022-03-05 00:20:38 -0300

    adding a form to route add

--- 12_13_14seccion/heroesApp-Angular-0.13.0/src/app/heroes/pages/agregar/agregar.component.ts
+++ 12_13_14seccion/heroesApp-Angular-0.13.0/src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Heroe, Publisher } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-agregar',
@@ -7,6 +8,26 @@ import { Component, OnInit } from '@angular/core';
   ]
 })
 export class AgregarComponent implements OnInit {
+
+  publishers = [
+    {
+      id: 'DC Comics',
+      desc: 'DC - Comics'
+    },
+    {
+      id: 'Marvel Comics',
+      desc: 'Marvel - Comics'
+    }
+  ];
+
+  heroe: Heroe = {
+    superhero: '',
+    alter_ego: '',
+    characters: '',
+    first_appearance: '',
+    publisher: Publisher.DCComics,
+    alt_img: '',
+  };
 
   constructor() { }
 
