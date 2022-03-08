59756c03d917e4720ce698ee36141ad16001b81c
jerosantamariai <jerosantamariai@gmail.com>
2022-03-08 01:01:32 -0300

    complete section 14

--- 12_13_14seccion/heroesApp-Angular-0.13.0/src/app/material/material.module.ts
+++ 12_13_14seccion/heroesApp-Angular-0.13.0/src/app/material/material.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
@@ -19,6 +20,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatAutocompleteModule,
     MatButtonModule,
     MatCardModule,
+    MatDialogModule,
     MatFormFieldModule,
     MatGridListModule,
     MatInputModule,
