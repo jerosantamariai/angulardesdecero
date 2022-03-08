12b9185113a7da484779299436d477cc2171e9b5
jerosantamariai <jerosantamariai@gmail.com>
2022-03-07 23:40:44 -0300

    adding delete endpoint and solve the the refresh of the image when its change

--- 12_13_14seccion/heroesApp-Angular-0.13.0/src/app/heroes/pipes/imagen.pipe.ts
+++ 12_13_14seccion/heroesApp-Angular-0.13.0/src/app/heroes/pipes/imagen.pipe.ts
@@ -2,7 +2,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { Heroe } from '../interfaces/heroes.interface';
 
 @Pipe({
-  name: 'imagen'
+  name: 'imagen',
+  pure: false
 })
 export class ImagenPipe implements PipeTransform {
 
--- 12_13_14seccion/heroesApp-Angular-0.13.0/src/app/heroes/services/heroes.service.ts
+++ 12_13_14seccion/heroesApp-Angular-0.13.0/src/app/heroes/services/heroes.service.ts
@@ -12,26 +12,29 @@ export class HeroesService {
 
   private baseUrl: string = environment.baseUrl;
 
-  constructor( private http: HttpClient ) { }
+  constructor(private http: HttpClient) { }
 
   getHeroes(): Observable<Heroe[]> {
-    return this.http.get<Heroe[]>(`${ this.baseUrl }/heroes`);
+    return this.http.get<Heroe[]>(`${this.baseUrl}/heroes`);
   }
 
-  getHeroePorId( id: string ):Observable<Heroe> {
-    return this.http.get<Heroe>(`${ this.baseUrl }/heroes/${ id }`);
+  getHeroePorId(id: string): Observable<Heroe> {
+    return this.http.get<Heroe>(`${this.baseUrl}/heroes/${id}`);
   }
 
-  getSugerencias( termino: string ): Observable<Heroe[]> {
-    return this.http.get<Heroe[]>(`${ this.baseUrl }/heroes?q=${ termino }&_limit=6`);
+  getSugerencias(termino: string): Observable<Heroe[]> {
+    return this.http.get<Heroe[]>(`${this.baseUrl}/heroes?q=${termino}&_limit=6`);
   }
 
-  agregarHeroe( heroe: Heroe ): Observable<Heroe> {
-    return this.http.post<Heroe>(`${ this.baseUrl }/heroes`, heroe)
+  agregarHeroe(heroe: Heroe): Observable<Heroe> {
+    return this.http.post<Heroe>(`${this.baseUrl}/heroes`, heroe);
   }
 
-  actualizarHeroe( heroe: Heroe ): Observable<Heroe> {
-    return this.http.put<Heroe>(`${ this.baseUrl }/heroes/${ heroe.id }`, heroe)
+  actualizarHeroe(heroe: Heroe): Observable<Heroe> {
+    return this.http.put<Heroe>(`${this.baseUrl}/heroes/${heroe.id}`, heroe);
   }
 
+  borrarHeroe(id: string): Observable<any> {
+    return this.http.delete<any>(`${this.baseUrl}/heroes/${id}`);
+  }
 }
