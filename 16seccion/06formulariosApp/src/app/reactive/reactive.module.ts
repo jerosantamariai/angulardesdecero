ab9c0a017b86f43e0cd74fff401c0bc9c23f0488
jerosantamariai <jerosantamariai@gmail.com>
2022-03-09 22:33:37 -0300

    configuring the routes of the app

--- 16seccion/06formulariosApp/src/app/reactive/reactive.module.ts
+++ 16seccion/06formulariosApp/src/app/reactive/reactive.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ReactiveRoutingModule } from './reactive-routing.module';
+import { BasicosComponent } from './basicos/basicos.component';
+import { DinamicosComponent } from './dinamicos/dinamicos.component';
+import { SwitchesComponent } from './switches/switches.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BasicosComponent,
+    DinamicosComponent,
+    SwitchesComponent
+  ],
   imports: [
     CommonModule,
     ReactiveRoutingModule
--- 16seccion/06formulariosApp/src/app/template/template-routing.module.ts
+++ 16seccion/06formulariosApp/src/app/template/template-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [
+import { BasicosComponent } from './basicos/basicos.component';
+import { DinamicosComponent } from './dinamicos/dinamicos.component';
+import { SwitchesComponent } from './switches/switches.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      { path: 'basicos', component: BasicosComponent },
+      { path: 'dinamicos', component: DinamicosComponent },
+      { path: 'switches', component: SwitchesComponent },
+      { path: '**', redirectTo: 'basicos' },
+    ]
+  }
 ];
 
 @NgModule({
