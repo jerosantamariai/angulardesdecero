4421bb9e94e3720a429d5adb71ca59294cb22015
jerosantamariai <jerosantamariai@gmail.com>
2022-04-12 19:38:03 -0400

    first validations for a reactive form

--- 16_17seccion/06formulariosApp/src/app/reactive/basicos/basicos.component.ts
+++ 16_17seccion/06formulariosApp/src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, 
+  // FormControl, 
+  FormGroup, 
+  Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -8,9 +12,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasicosComponent implements OnInit {
 
-  constructor() { }
+  // Usado para cuando no usamos FormBuilder
+  // miFormulario: FormGroup = new FormGroup({
+  //   nombre: new FormControl('RTX 4080ti'),
+  //   precio: new FormControl(2345),
+  //   cantidad: new FormControl(8)
+  // });
+
+  // Usando FormBuilder
+  miFormulario: FormGroup = this.fb.group({
+    nombre: [ , [ Validators.required, Validators.minLength(3) ] ],
+    precio: [ , [ Validators.required, Validators.min(0) ] ],
+    cantidad: [ , [ Validators.required, Validators.min(0) ] ]
+  });
+
+  constructor( private fb: FormBuilder ) { }
 
   ngOnInit(): void {
+  }
+
+  campoValido( campo: string) {
+    return this.miFormulario.controls[campo].errors && this.miFormulario.controls[campo].touched;
+  }
+
+  guardar() {
+    console.log(this.miFormulario)
   }
 
 }
