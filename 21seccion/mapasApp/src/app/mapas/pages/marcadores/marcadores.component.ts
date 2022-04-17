0db4ade5f5fb52f4b158eca6289334031ec78f03
jerosantamariai <jerosantamariai@gmail.com>
2022-04-17 03:53:04 -0400

    advance until my first marker

--- 21seccion/mapasApp/src/app/mapas/pages/marcadores/marcadores.component.ts
+++ 21seccion/mapasApp/src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -1,16 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild, AfterViewInit } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'app-marcadores',
   templateUrl: './marcadores.component.html',
   styles: [
+    `
+      .map-container {
+        width: 100vw;
+        height: 100vh;
+      }
+    `
   ]
 })
-export class MarcadoresComponent implements OnInit {
+export class MarcadoresComponent implements AfterViewInit {
+
+  @ViewChild('mapa') divMapa!: ElementRef;
+  mapa!: mapboxgl.Map;
+  zoomLevel: number = 13;
+  center: [ number, number ] = [-70.673676, -33.447487]
 
   constructor() { }
+  
+  ngAfterViewInit(): void {
 
-  ngOnInit(): void {
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v11', // style URL
+      center: this.center, // starting position [lng, lat]
+      zoom: this.zoomLevel // starting zoom      
+    });
+
+    const markerHtml: HTMLElement = document.createElement('div');
+    markerHtml.innerHTML = 'Yo aqui'
+
+    new mapboxgl.Marker({
+      element: markerHtml,
+    })
+      .setLngLat( this.center )
+      .addTo( this.mapa );
+
   }
 
 }
--- 21seccion/mapasApp/src/app/mapas/pages/zoom-range/zoom-range.component.ts
+++ 21seccion/mapasApp/src/app/mapas/pages/zoom-range/zoom-range.component.ts
@@ -1,16 +1,84 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit, OnDestroy } from '@angular/core';
+import * as mapboxgl from 'mapbox-gl';
+
 
 @Component({
   selector: 'app-zoom-range',
   templateUrl: './zoom-range.component.html',
   styles: [
+    `
+      .map-container {
+        width: 100vw;
+        height: 100vh;
+      }
+
+      .row {
+        background-color: white;
+        position: fixed;
+        bottom: 50px;
+        left: 50px;
+        padding: 10px;
+        border-radius: 5px;
+        z-index: 999;
+        width: 400px;
+      }
+    `
   ]
 })
-export class ZoomRangeComponent implements OnInit {
+export class ZoomRangeComponent implements AfterViewInit, OnDestroy {
+
+  @ViewChild('mapa') divMapa!: ElementRef;
+  mapa!: mapboxgl.Map;
+  zoomLevel: number = 9;
+  center: [ number, number ] = [-70.673676, -33.447487]
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnDestroy(): void {
+    this.mapa.off( 'zoom', () => {});
+    this.mapa.off( 'zoomend', () => {});
+    this.mapa.off( 'move', () => {});
+  }
+
+  ngAfterViewInit(): void {
+
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v11', // style URL
+      center: this.center, // starting position [lng, lat]
+      zoom: this.zoomLevel // starting zoom
+    });
+
+    this.mapa.on('zoom', () => {
+      this.zoomLevel = this.mapa.getZoom();
+    });
+
+    this.mapa.on('zoomend', () => {
+      if( this.mapa.getZoom() > 18 ) {
+        this.mapa.zoomTo(18);
+      }
+    });
+
+    this.mapa.on('move', (event) => {
+      const target = event.target;
+      const { lng, lat } = target.getCenter();
+      this.center = [ lng, lat ];
+    });
+
+  }
+
+  zoomIn() {
+    this.mapa.zoomIn();
+    this.zoomLevel = this.mapa.getZoom();
+  }
+
+  zoomOut() {
+    this.mapa.zoomOut();
+    this.zoomLevel = this.mapa.getZoom();
+  }
+
+  zoomCambio( valor: string ) {
+    this.mapa.zoomTo( Number( valor ) );
   }
 
 }
