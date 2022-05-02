d84f521499d9e856f2b9da31085721db5d365659
jerosantamariai <jerosantamariai@gmail.com>
2022-05-01 23:02:03 -0400

    some advance

---
+++ 24seccion/authApp/authFront/src/app/auth/services/auth.service.ts
@@ -0,0 +1,30 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+
+import { AuthResponse, Usuario } from 'src/app/interfaces/interfaces';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private baseUrl: string = environment.baseUrl;
+  private _usuario!: Usuario;
+
+  get usuario() {
+    return this._usuario;
+  }
+
+  constructor( private http: HttpClient) { }
+
+  login( email: string, password: string ) {
+    
+    const url = `${ this.baseUrl }/auth/login`;
+    const body = { email, password };
+    
+    return this.http.post<AuthResponse>(url, body)
+
+  }
+
+}
